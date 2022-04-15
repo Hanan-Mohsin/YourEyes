4b7ba00eab8416a8f95156d72f5da4cc12ebd39f
Naomi <naomitibebe19@gmail.com>
2022-04-15 22:36:34 +0300

    changes made to translation

--- youreyes_frontend/lib/translation/model/translation.dart
+++ youreyes_frontend/lib/translation/model/translation.dart
@@ -2,7 +2,12 @@ class Translation{
 
 
   final String text;
-  final Map<String,String> audioMap = {'trial':'assets/Background-1.mp3','right':'assets/Voice 004.mp3','left': 'assets/leftAudio.mp3'};
+  final Map<String,String> audioMap = {'trial':'assets/Background-1.mp3',
+                                      'right':'assets/Voice 004.mp3',
+                                      'left': 'assets/Voice 004.mp3',
+                                      'northwest':'assets/Voice 004.mp3',
+                                      'southwest':'assets/Voice 004.mp3',
+                                      'arrived':'assets/Voice 004.mp3'};
 
   Translation({required this.text});
 }
