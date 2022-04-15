e62901e00e48c398431cf76f7ae6e1cc38720729
Kidist Kebede <Kidist.k.demissie@gmail.com>
2022-04-15 20:19:16 +0300

    changes made to destination input page

--- youreyes_frontend/lib/destination/widget/destination_input_page.dart
+++ youreyes_frontend/lib/destination/widget/destination_input_page.dart
@@ -1,5 +1,6 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:http/http.dart';
 import 'package:just_audio/just_audio.dart';
 import 'package:youreyes_frontend/destination/model/model.dart';
@@ -27,7 +28,8 @@ class _DestinationInputPageState extends State<DestinationInputPage>{
   SpeechToText _speechToText = SpeechToText();
   bool _speechEnabled = false;
   String _lastWords = '';
- 
+  bool stopped = false;
+  AudioPlayer player = AudioPlayer();
    @override
   void initState() {
 
@@ -57,13 +59,18 @@ class _DestinationInputPageState extends State<DestinationInputPage>{
   void _stopListening() async {
     await _speechToText.stop();
     setState(() {});
-     
+    
+    if(!stopped){  
+      if(_lastWords !=''){
+        print('fkjkfjkfjkfjkfjkf');
+        print(_lastWords);
+        setState(() {
+          stopped = true;
+        });
+        await _getDestination(_lastWords);
+        
       
-    if(_lastWords !=''){
-      print(_lastWords);
-      await _getDestination("Addis Ababa Institute of Technology");
-      
-     
+      }
     }
   }
     
@@ -77,7 +84,7 @@ class _DestinationInputPageState extends State<DestinationInputPage>{
       
     });
     
-       _stopListening();
+      _stopListening();
     
     // print(_lastWords);
     //  await  _getDestination("Addis Ababa Institute of Technology");
@@ -94,7 +101,16 @@ class _DestinationInputPageState extends State<DestinationInputPage>{
   
   }
 
+  Future _playAudio(String pathToAudio)async{
+   
+   
+    await player.setAsset(pathToAudio);
+    player.play();
+   
+  }
+
   Future _getDestination(String query) async{
+    if(query != ''){
     Map<String,dynamic> result = await _destinationService.getDestination(query);
     List<dynamic> _steps = await _destinationService.getRoute(result["destination"],result["address"]);
     
@@ -102,15 +118,19 @@ class _DestinationInputPageState extends State<DestinationInputPage>{
         _instructions = _instructionService.getInstruction(_steps);
       
      });
+    }
       final cameras = await availableCameras();
       final firstCamera = cameras.first;
       // Get a specific camera from the list of available cameras.
+     
        navigatorKey.currentState!.pop();
+      
        await navigatorKey.currentState!.push(
         MaterialPageRoute(
-          builder: (context) => TakePicturePage(instructions: _instructions, camera: firstCamera)
+          builder: (context) => TakePicturePage(instructions: _instructions, camera: firstCamera, player: player)
         ),
       );
+       
     
   }
 
@@ -123,32 +143,32 @@ class _DestinationInputPageState extends State<DestinationInputPage>{
       navigatorKey: navigatorKey,
       home:Scaffold(
         body: Container(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+          
+          width:MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+         // child: Center(
+            child: ListView(
+             // mainAxisSize: MainAxisSize.max,
+              //mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                FloatingActionButton(
+                 ElevatedButton(
+
                   onPressed:
                       // If not yet listening for speech start, otherwise stop
                     _speechToText.isNotListening ? _startListening : _stopListening,
-                    tooltip: 'Listen',
-                    child: Icon(_speechToText.isNotListening ? Icons.mic_off : Icons.mic),
+                  //  tooltip: 'Listen',
+                    child: Icon(_speechToText.isNotListening ? Icons.mic_off : Icons.mic, size:90),
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: Size(MediaQuery.of(context).size.width,(MediaQuery.of(context).size.height/2) - 5)
+                    )
                  ),
+                  
+                  
 
                   SizedBox(
                     height: 10,
-                  ),
-                  ElevatedButton(
-                    onPressed: ()async{
-                      //sawait getDestination('Afarensis International Hotel');
-                    }, 
-                    child: Text('settt')),
-                     SizedBox(
-                    height: 10,
-                  ),
-
-                  Container(
-                    padding: EdgeInsets.all(16),
+                    child: Container(
+                  
                     child: Text(
                       _speechToText.isListening
                           ? '$_lastWords'
@@ -158,9 +178,35 @@ class _DestinationInputPageState extends State<DestinationInputPage>{
                               : 'Speech not available',
                     ),
                   ),
-              ]
+                  ),
+                ElevatedButton(
+                    onPressed: ()async{
+                      await _getDestination('');
+                       //await _playAudio('assets/Voice 004.mp3');
+                    }, 
+                    child: Text('Skip',style: TextStyle(fontSize:46)),
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: Size(MediaQuery.of(context).size.width,(MediaQuery.of(context).size.height/2)-5)
+                    )
+                    ),
+                    
+                     
+                  
+                 
+              //     Container(
+              //       padding: EdgeInsets.all(16),
+              //       child: Text(
+              //         _speechToText.isListening
+              //             ? '$_lastWords'
+                        
+              //             : _speechEnabled
+              //                 ? (_lastWords == '' ? 'Tap the microphone to start listening...':'$_lastWords')
+              //                 : 'Speech not available',
+              //       ),
+              //     ),
+               ]
             ),
-          )
+         // )
         )
       )
     );
