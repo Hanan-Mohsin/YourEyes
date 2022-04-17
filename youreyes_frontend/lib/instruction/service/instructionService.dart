5263c878996b87e6961cfb1011f3c8ee8ff13e71
Hanan Mohsin <hananmohsin967088@gmail.com>
2022-04-17 21:28:08 +0300

    instruction service updated

--- youreyes_frontend/lib/instruction/service/instructionService.dart
+++ youreyes_frontend/lib/instruction/service/instructionService.dart
@@ -20,25 +20,72 @@ class InstructionService{
     return instructions;
     
   }
+
+ 
   // instructions and address will be passed from the previous pages
 
-  List<Instruction> deliverInstruction(List<Instruction> instructions,Position currentPosition,AudioPlayer player,int instLength){
+ 
+  // Future<List<Instruction>> deliverInstruction(List<Instruction> instructions,Position currentPosition,AudioPlayer player,int instLength) async {
+    
+  //   if(instructions.length == instLength){
+  //    print('Instruction to be delivered: ${instructions[0].instruction}');
+  //    await _tService.playAudio(player, Translation(text: instructions[0].instruction));
+  //     instructions.removeAt(0);  
+   
+
+  //  }
+  //  if(instructions.length > 1){
+  //     if((Geolocator.distanceBetween(currentPosition.latitude, currentPosition.longitude, instructions[0].checkpoint['latitude'],instructions[0].checkpoint['longitude'])).abs() < 0.05){
+  //      print(instructions[0].distance - Geolocator.distanceBetween(currentPosition.latitude, currentPosition.longitude, instructions[1].checkpoint['latitude'],instructions[1].checkpoint['longitude']));
+  //       print(instructions[0].instruction);  //This is instruction is delivered
+  //       await _tService.playAudio(player, Translation(text: instructions[0].instruction));
+  //       instructions.removeAt(0);    
+  //   }
+    
+  //  }else{
+  //    if((instructions[0].distance - Geolocator.distanceBetween(currentPosition.latitude, currentPosition.longitude, instructions[0].checkpoint['latitude'],instructions[0].checkpoint['longitude'])).abs() < 0.05){
+  //      print(instructions[0].distance - Geolocator.distanceBetween(currentPosition.latitude, currentPosition.longitude, instructions[0].checkpoint['latitude'],instructions[0].checkpoint['longitude']));
+  //       print(instructions[0].instruction);  //This is instruction is delivered
+  //       await _tService.playAudio(player, Translation(text: instructions[1].instruction));
+  //       instructions.removeAt(0);    
+  //   }
+  //  }
+  //  return instructions;
+ 
+  //  }
+  
+
+  // For mocking location
+
+  Future<List<Instruction>> deliverInstruction(List<Instruction> instructions,Map<String,double> currentPosition,AudioPlayer player,int instLength) async {
     
     if(instructions.length == instLength){
      print('Instruction to be delivered: ${instructions[0].instruction}');
+     await _tService.playAudio(player, Translation(text: instructions[0].instruction));
       instructions.removeAt(0);  
-   //  _tService.playAudio(player, Translation(text: "right"));  //instruction in place of trail
+   
 
-   }
-   if(instructions.length > 1){
-      if((instructions[0].distance - Geolocator.distanceBetween(currentPosition.latitude, currentPosition.longitude, instructions[1].checkpoint['latitude'],instructions[1].checkpoint['longitude'])).abs() < 0.5){
-       print(instructions[0].distance - Geolocator.distanceBetween(currentPosition.latitude, currentPosition.longitude, instructions[1].checkpoint['latitude'],instructions[1].checkpoint['longitude']));
-        print(instructions[1].instruction);  //This is instruction is delivered
-        instructions.removeAt(0);    
+   }else{
+     print(instructions.length);
+      print((instructions[0].distance - Geolocator.distanceBetween(currentPosition['latitude'], currentPosition['longitude'], instructions[0].checkpoint['latitude'],instructions[0].checkpoint['longitude'])).abs());
+      if((Geolocator.distanceBetween(currentPosition['latitude'], currentPosition['longitude'], instructions[0].checkpoint['latitude'],instructions[0].checkpoint['longitude'])).abs() < 0.5){
+      
+        print(instructions[0].instruction); 
+        String inst = instructions[0].instruction; //This is instruction is delivered
+        instructions.removeAt(0); 
+        await _tService.playAudio(player, Translation(text: inst));
+           
     }
     
    }
+  //  else{
+  //   if((instructions[0].distance - Geolocator.distanceBetween(currentPosition['latitude'], currentPosition['longitude'], instructions[0].checkpoint['latitude'],instructions[0].checkpoint['longitude'])).abs() < 0.5){
+  //     print(instructions[0].instruction);  //This is instruction is delivered
+  //     await _tService.playAudio(player, Translation(text: instructions[0].instruction));
+  //     instructions.removeAt(0);  
+  //  }
+  // }
    return instructions;
  
    }
-  }
+}
